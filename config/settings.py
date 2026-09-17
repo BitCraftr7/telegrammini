3fecb70a3de06ae113899048c38069cda9f81d6e
BitCraftr7 <kalibaevabdirasul@gmail.com>
2026-09-17 16:36:04 +0500

    CSRF trusted origins qoshildi

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['.onrender.com', '127.0.0.1', 'localhost']
 
-# Application definition
+CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com']# Application definition
 
 INSTALLED_APPS = [
     'daphne',
